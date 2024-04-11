66b81421367a8661591e6e2d0c2965e2123eb793
MauricioNC <mauricionc19@gmail.com>
2024-04-11 17:15:38 -0600

    feat: Comments model and controller created. Updated routes from resources to single endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :features
+      get 'features', to: 'features#index'
+      post '/features/:id/comments', to: 'comments#create'
     end
   end
 end
